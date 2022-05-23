60adda164f228572e7569461ff56a3ed0085b491
Kai Larikka <kai.larikka@kolumbus.fi>
2022-05-24 01:57:33 +0300

    Ex level1 first test

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,13 +1,22 @@
 export class Board {
   width;
   height;
+  board;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.board = new Array(width*height).fill(".");
   }
 
   toString() {
-    return "TODO";
+    let print = "";
+    for(let i = 0; i < this.height; i++) {
+      for(let j = 0; j < this.width; j++) {
+	print += this.board[i+j];
+      }
+      print += "\n";
+    }
+    return print;
   }
 }
