806ad7824e6a876399481092868fa7d818e54f3e
Kai Larikka <kai.larikka@kolumbus.fi>
2022-05-24 22:58:25 +0300

    Add more tests and refactor game design

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -2,18 +2,59 @@ export class Board {
   width;
   height;
   board;
+  fallingBlock;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.board = new Array(width*height).fill(".");
   }
+	
+  drop(block) {
+    if(this.fallingBlock) throw "already falling";
+    this.fallingBlock = [[1], block.color, true];
+    this.draw();
+  }
+
+  tick() {
+    this.undraw();
+    this.updateBlockPosition();
+    this.draw();
+  }
+
+  hasFalling() {
+    if(this.fallingBlock[2]) return true;
+    return false;
+  }
+
+  draw() {
+    for(let i = 0; i < this.fallingBlock.length; i++) {
+      this.board[this.fallingBlock[0][i]] = this.fallingBlock[1];
+    }
+  }
+
+  undraw() {
+    for(let i = 0; i < this.fallingBlock.length; i++) {
+      this.board[this.fallingBlock[0][i]] = ".";
+    }
+  }
+
+  updateBlockPosition() {
+    for(let i = 0; i < this.fallingBlock.length; i++) {
+      const updatedPosition = this.fallingBlock[0][i] + this.width;
+      if(updatedPosition >= this.board.length) {
+        this.fallingBlock[2] = false;
+        return;
+      }
+      this.fallingBlock[0][i] += this.width;
+    }
+  }
 
   toString() {
     let print = "";
     for(let i = 0; i < this.height; i++) {
       for(let j = 0; j < this.width; j++) {
-	print += this.board[i+j];
+        print += this.board[i*this.height+j];
       }
       print += "\n";
     }
