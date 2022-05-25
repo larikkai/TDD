c81259ad363e991a822c4454cc61a8f841962d87
Kai Larikka <kai.larikka@kolumbus.fi>
2022-05-25 21:45:49 +0300

    Refactor rotate

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -3,52 +3,37 @@ export class RotatingShape {
   shape;
 
   constructor(shapeString) {
-    this.width = shapeString.indexOf("\n");
-    let string = shapeString.replace(/\s+/g, "");
-    this.shape = string.split("");
+    if(typeof shapeString === "string") {
+      this.width = shapeString.indexOf("\n");
+      let string = shapeString.replace(/\s+/g, "");
+      this.shape = string.split("");
+    } else {
+      this.width = Math.sqrt(shapeString.length);
+      this.shape = shapeString;
+    }
   }
 
   rotateRight() {
-    const dest = [...this.shape];
-    const temp = [...this.shape];
-    this.rotate(temp, dest);
-    return this.toString(dest);
-  }
-
-  rotateLeft() {
-    const dest = [...this.shape];
-    for(let i = 0; i < 3; i++) {
-      let temp = [...dest];
-      this.rotate(temp, dest);
-    }
-    return this.toString(dest);
-  }
-
-  rotate(src, dest) {
+    const dest = new Array(9);
     const n = this.width;
     for(let i = 0; i < n; i++) {
       for(let j = 0; j < n; j++) {
-        dest[((i*n+j)%n)*n+n-1-i] = src[i*n+j];
+        dest[((i*n+j)%n)*n+n-1-i] = this.shape[i*n+j];
       }
     }
+    return new RotatingShape(dest);
   }
 
-  rotate2(src, dest) {
-    const table = this.width === 3 ? this.rotateTable3 : this.rotateTable5;
-    for(let i = 0; i < this.shape.length; i++) {
-      const nextPos = table[i];
-      const letter = src[i];
-      dest[nextPos] = letter;
-    }
+  rotateLeft() {
+    return this.rotateRight().rotateRight().rotateRight();
   }
 
-  toString(source) {
-    const arr = source ?? this.shape;
+  toString() {
     const n = this.width;
     let print = "";
     for(let i = 0; i < n; i++) {
       for(let j = 0; j < n; j++) {
-        print += arr[i*n+j];
+        print += this.shape[i*n+j];
       }
       print += "\n";
     }
