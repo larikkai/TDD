7dea7c5ffc1e55330c2ca21912dc31843fbe4856
Kai Larikka <kai.larikka@kolumbus.fi>
2022-05-29 15:13:40 +0300

    Add I_SHAPE tests and Tetromino class constructor

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -18,21 +18,33 @@ export class Tetromino {
     }
   };
 
-  static I_SHAPE = {
-    toString: function() {
-      return [
-      [".", ".", ".", ".", "."],
-      [".", ".", ".", ".", "."],
-      ["I", "I", "I", "I", "."],
-      [".", ".", ".", ".", "."],
-      [".", ".", ".", ".", "."],
-      ].map(row => row.join("")).join("\n") + "\n";
-    },
-    rotateRight: function() {
-      return new RotatingShape(this.toString()).rotateRight();
-    },
-    rotateLeft: function() {
-      return new RotatingShape(this.toString()).rotateRight();
-    }
-  };
+  static I_SHAPE = new Tetromino([
+    ".....\n.....\nIIII.\n.....\n.....",
+    "..I..\n..I..\n..I..\n..I..\n....."], 0);
+
+  #shape;
+  #shapes;
+  rotation;
+
+  constructor(...args) {
+    this.rotation = args[1];
+    this.#shape = args[0][this.rotation];
+    this.#shapes = args[0];
+  }
+
+  toString() {
+     return this.#shape.toString() + "\n";
+  }
+
+  rotateRight() {
+    let nextRotation = this.rotation + 1;
+    if(nextRotation === this.#shapes.length) nextRotation = 0;
+    return new Tetromino(this.#shapes, nextRotation);
+  }
+
+  rotateLeft() {
+    let nextRotation = this.rotation - 1;
+    if(nextRotation < 0) nextRotation = this.#shapes.length - 1;
+    return new Tetromino(this.#shapes, nextRotation);
+  }
 };
