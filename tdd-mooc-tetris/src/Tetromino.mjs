b334d463b07ab560228a3b587c72718873653a6c
Kai Larikka <kai.larikka@kolumbus.fi>
2022-05-29 15:22:56 +0300

    Change rotation to private and add semicolons

--- tdd-mooc-tetris/src/Tetromino.mjs
+++ tdd-mooc-tetris/src/Tetromino.mjs
@@ -14,27 +14,27 @@ export class Tetromino {
 
   #shape;
   #shapes;
-  rotation;
+  #rotation;
 
   constructor(...args) {
-    this.rotation = args[1];
-    this.#shape = args[0][this.rotation];
+    this.#rotation = args[1];
+    this.#shape = args[0][this.#rotation];
     this.#shapes = args[0];
-  }
+  };
 
   toString() {
      return this.#shape.toString() + "\n";
-  }
+  };
 
   rotateRight() {
-    let nextRotation = this.rotation + 1;
+    let nextRotation = this.#rotation + 1;
     if(nextRotation === this.#shapes.length) nextRotation = 0;
     return new Tetromino(this.#shapes, nextRotation);
-  }
+  };
 
   rotateLeft() {
-    let nextRotation = this.rotation - 1;
+    let nextRotation = this.#rotation - 1;
     if(nextRotation < 0) nextRotation = this.#shapes.length - 1;
     return new Tetromino(this.#shapes, nextRotation);
-  }
+  };
 };
