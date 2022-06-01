05fef6064dd7fd87af2765190f75191c8c849fd6
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-01 14:22:36 +0300

    Add FallingTetrominoes tests and relevant code

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -10,20 +10,24 @@ export class Board {
 
   constructor(...args) {
     [this.width, this.height] = args;
-    this.board = new Array(this.width * this.height).fill(false);
+    this.board = new Array((this.width + 1) * this.height)
+      .fill(new Block("."))
+      .fill(new Block("#", true), this.width * this.height);
   }
 
   drop(block) {
     if (this.fallingBlock) throw "already falling";
     this.currentPosition = Math.floor((this.width - 1) / 2);
     this.fallingBlock = block;
+    this.updateCoordinates();
     this.draw();
   }
 
   tick() {
-    this.updateFallingBlockPosition();
-    if (!this.fallingBlock) return;
+    this.validate();
+    if (this.fallingBlock === null) return;
     this.undraw();
+    this.currentPosition += this.width;
     this.draw();
   }
 
@@ -32,30 +36,43 @@ export class Board {
   }
 
   draw() {
-    this.updateCoordinates();
     this.coordinates.forEach((value) => {
-      this.board[value] = this.fallingBlock.getColor();
+      this.board[this.currentPosition + value] = new Block(
+        this.fallingBlock.getColor()
+      );
     });
   }
 
   undraw() {
     this.coordinates.forEach((value) => {
-      this.board[value] = false;
+      this.board[this.currentPosition + value] = new Block(".");
     });
+  }
+
+  validate() {
+    const newPos = this.currentPosition + this.width;
+    if (
+      this.coordinates.some((value) => this.board[newPos + value].isTaken())
+    ) {
+      this.coordinates.forEach((value) =>
+        this.board[this.currentPosition + value].setTaken()
+      );
+      this.fallingBlock = null;
+    }
   }
 
   updateCoordinates() {
     this.coordinates = this.fallingBlock
       .getCoordinates()
-      .map((row, r) =>
-        row.map((column, c) => r * this.width + this.currentPosition + column)
-      )
+      .map((row, r) => row.map((column, c) => r * this.width + column))
       .flat();
   }
 
   updateFallingBlockPosition() {
-    const newPosition = this.currentPosition + this.width;
-    if (this.notValidPosition(newPosition)) {
+    const newPos = this.currentPosition + this.width;
+    if (
+      !this.coordinates.some((value) => this.notValidPosition(newPos - value))
+    ) {
       this.fallingBlock = null;
       return;
     }
@@ -74,16 +91,17 @@ export class Board {
   }
 
   taken(position) {
-    return this.board[position];
+    return this.board[position] && !this.coordinates.includes(position);
   }
 
   toString() {
     let print = "";
     for (let i = 0; i < this.height; i++) {
       for (let j = 0; j < this.width; j++) {
-        const taken = this.board[i * this.width + j];
+        /*const taken = this.board[i * this.width + j];
         if (!taken) print += ".";
-        else print += taken;
+        else print += taken;*/
+        print += this.board[i * this.width + j].getColor();
       }
       print += "\n";
     }
