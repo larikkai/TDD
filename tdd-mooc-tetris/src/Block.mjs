e0455438fd370217a227437b592e8de708e852a4
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-01 14:21:50 +0300

    Add state, getter and setters

--- tdd-mooc-tetris/src/Block.mjs
+++ tdd-mooc-tetris/src/Block.mjs
@@ -1,12 +1,22 @@
 export class Block {
   #color;
+  #taken;
 
   constructor(...args) {
-    [this.#color] = args;
+    this.#color = args[0];
+    this.#taken = args[1] ?? false;
   }
 
   getColor() {
     return this.#color;
+  }
+
+  isTaken() {
+    return this.#taken;
+  }
+
+  setTaken() {
+    this.#taken = true;
   }
 
   getCoordinates() {
