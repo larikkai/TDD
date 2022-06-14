6d081b5c40b8e28d9c9a7fa3a998cfd0db1dee8c
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-14 18:12:02 +0300

    Add ex4

--- tdd-mooc-new-js-project/src/example.mjs
+++ tdd-mooc-new-js-project/src/example.mjs
@@ -1,3 +1,16 @@
+import * as fs from "fs";
+
 export function sum(a, b) {
   return a + b;
+}
+
+const nine = 9;
+
+export function multiplyByNine(path, number, date) {
+  const result = number * nine * Math.floor(Math.random() * 2);
+  const text = `Calculated: date\n result: ${number} times 9 = ${result}\n`;
+  fs.writeFile(path, text, (err) => {
+    if (err) throw err;
+  });
+  return result;
 }
