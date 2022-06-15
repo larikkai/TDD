88205292d9c0913a673cb640cbcbb5691926490c
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-15 22:46:41 +0300

    Refactor operations by item.name

--- tdd-mooc-legacy-code/src/gilded_rose.js
+++ tdd-mooc-legacy-code/src/gilded_rose.js
@@ -17,27 +17,23 @@ class Shop {
       const ab = "Aged Brie";
       const taf = "Backstage passes to a TAFKAL80ETC concert";
       const rag = "Sulfuras, Hand of Ragnaros";
-      if (name === ab && quality < 50) quality++;
-      if (name === taf && quality < 50) {
-        quality++;
-        if (sellIn < 11 && quality < 50) quality++;
-        if (sellIn < 6 && quality < 50) quality++;
-      }
-      if (quality > 0 && name !== ab && name !== taf && name !== rag) quality--;
-      if (name !== rag) {
-        sellIn--;
-      }
-      if (sellIn >= 0) {
-        this.items[i] = { name, sellIn, quality };
-        return this.items;
-      }
       if (name === ab) {
         if (quality < 50) quality++;
-        this.items[i] = { name, sellIn, quality };
-        return this.items;
+        sellIn--;
+        if (sellIn < 0 && quality < 50) quality++;
+      } else if (name === taf) {
+        if (quality < 50) quality++;
+        if (sellIn < 11 && quality < 50) quality++;
+        if (sellIn < 6 && quality < 50) quality++;
+        sellIn--;
+        if (sellIn < 0) quality = 0;
+      } else if (name === rag) {
+        if (sellIn >= 0 && quality > 0) quality--;
+      } else {
+        if (quality > 0) quality--;
+        sellIn--;
+        if (sellIn < 0 && quality > 0) quality--;
       }
-      if (name === taf) quality = 0;
-      if (name !== rag && quality > 0) quality--;
       this.items[i] = { name, sellIn, quality };
     }
     return this.items;
