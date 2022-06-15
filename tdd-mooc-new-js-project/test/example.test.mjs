3188764f39c217e924149dffa2ef306c8ed1658a
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-15 08:12:58 +0300

    Ex3 test

--- tdd-mooc-new-js-project/test/example.test.mjs
+++ tdd-mooc-new-js-project/test/example.test.mjs
@@ -1,8 +1,41 @@
 import { expect } from "chai";
-import { sum } from "../src/example.mjs";
+import { sum, multiplyByNine } from "../src/example.mjs";
+import * as fs from "fs";
+
+function getResults(path, number, date) {
+  const distinct = new Set();
+  for (let i = 0; i < 10; i++) {
+    distinct.add(multiplyByNine(path, number, date));
+  }
+  return [...distinct].every((result) => {
+    return result % 9 === 0;
+  });
+}
+
+function getFileStats(path) {
+  fs.stat(path, (err, stat) => {});
+}
 
 describe("Example test fixture", () => {
+  const path = "./test/temp.txt";
+
+  after(() => {
+    fs.unlink(path, (err) => {
+      if (err) throw err;
+    });
+  });
+
   it("Example test", () => {
     expect(sum(1, 2)).to.equal(3);
+  });
+
+  it("Result is written to file", () => {
+    multiplyByNine(path, 2, new Date());
+    expect(getFileStats(path)).not.to.throw;
+  });
+
+  it("Result 0 or multiplied by 9", () => {
+    const result = getResults(path, 2, new Date());
+    expect(result).to.be.true;
   });
 });
