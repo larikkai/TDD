e389a996a26ac63e2280df013dc3b44d36ba480d
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-24 23:35:50 +0300

    Add still pattern tests

---
+++ tdd-mooc-gonway/test/Still.test.mjs
@@ -0,0 +1,26 @@
+import { expect } from "chai";
+import { Grid } from "../src/Grid.mjs";
+
+function distinct(s, n) {
+  const distinct = new Set();
+  for (let i = 0; i < 10; i++) {
+    const shape = new Grid(s, n, i);
+    distinct.add(shape.toString());
+  }
+  return distinct.size;
+}
+
+describe("Still lifes!", () => {
+  let grid;
+
+  it("Block with one iteration", () => {
+    let iterations = 1;
+    const grid = new Grid("bbbb$boob$boob$bbbb!", 4, iterations);
+    expect(grid.toString()).to.equal("bbbb$boob$boob$bbbb!");
+  });
+
+  it("Blinker has one distinct shape", () => {
+    const shape = "bbbb$boob$boob$bbbb!";
+    expect(distinct(shape, 4)).to.equal(1);
+  });
+});
