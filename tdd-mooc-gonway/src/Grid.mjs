521c4d0e918a987e769ce41833139c5b50ee9e0e
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-24 23:21:55 +0300

    Add Blinker tests and code

--- tdd-mooc-gonway/src/Grid.mjs
+++ tdd-mooc-gonway/src/Grid.mjs
@@ -2,26 +2,27 @@ export class Grid {
   #grid;
 
   constructor(shape, width, iterations) {
-    const arr = shape.split("");
-    const newGrid = shape.split("");
+    let newGrid = shape.split("");
     while (iterations !== 0) {
       iterations--;
+      const arr = shape.split("");
       const n = arr.length;
       for (let i = 0; i < n; i++) {
         let ne = 0;
-        if (i < n - 1 && arr[i + 1] === "o") ne++;
-        if (i > 0 && arr[i - 1] === "o") ne++;
-        if (i < n - width - 2 && arr[i + width + 2] === "o") ne++;
-        if (i < n - width - 1 && arr[i + width + 1] === "o") ne++;
-        if (i < n - width && arr[i + width] === "o") ne++;
-        if (i > width + 2 && arr[i - width - 2] === "o") ne++; 
-        if (i > width + 1 && arr[i - width - 1] === "o") ne++;
-        if (i > width && arr[i - width] === "o") ne++;
+        if (i < n - 1 && newGrid[i + 1] === "o") ne++;
+        if (i > 0 && newGrid[i - 1] === "o") ne++;
+        if (i < n - width - 2 && newGrid[i + width + 2] === "o") ne++;
+        if (i < n - width - 1 && newGrid[i + width + 1] === "o") ne++;
+        if (i < n - width && newGrid[i + width] === "o") ne++;
+        if (i > width + 2 && newGrid[i - width - 2] === "o") ne++; 
+        if (i > width + 1 && newGrid[i - width - 1] === "o") ne++;
+        if (i > width && newGrid[i - width] === "o") ne++;
 
-        if (arr[i] === "o" && ne < 2) newGrid[i] = "b";
-        if (arr[i] === "o" && ne > 3) newGrid[i] = "b";
-        if (arr[i] === "b" && ne === 3) newGrid[i] = "o";
+        if (newGrid[i] === "o" && ne < 2) arr[i] = "b";
+        if (newGrid[i] === "o" && ne > 3) arr[i] = "b";
+        if (newGrid[i] === "b" && ne === 3) arr[i] = "o";
       }
+      newGrid = arr;
     }
     this.#grid = newGrid;
   }
--- tdd-mooc-gonway/test/LivingGrid.test.mjs
+++ tdd-mooc-gonway/test/LivingGrid.test.mjs
@@ -1,13 +1,32 @@
 import { expect } from "chai";
 import { Grid } from "../src/Grid.mjs";
-import { Shape } from "../src/Shape.mjs";
 
-describe("The grid to enables life", () => {
+function distinct(s, n) {
+  const distinct = new Set();
+  for (let i = 0; i < 10; i++) {
+    const shape = new Grid(s, n, i);
+    distinct.add(shape.toString());
+  }
+  return distinct.size;
+}
+
+describe("There is life!", () => {
   let grid;
 
-  it("The grid starts empty", () => {
+  it("Blinker lives with one iteration", () => {
     let iterations = 1;
     const grid = new Grid("bbb$ooo$bbb!", 3, iterations);
     expect(grid.toString()).to.equal("bob$bob$bob!");
+  });
+  
+  it("Blinker lives with two iteration", () => {
+    let iterations = 2;
+    const grid = new Grid("bbb$ooo$bbb!", 3, iterations);
+    expect(grid.toString()).to.equal("bbb$ooo$bbb!");
+  });
+
+  it("Blinker has two distinct shapes", () => {
+    const shape = "bbb$ooo$bbb!";
+    expect(distinct(shape, 3)).to.equal(2);
   });
 });
