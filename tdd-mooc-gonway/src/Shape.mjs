fbee33db87d1ad648c0814b179ebf10cc812311e
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-24 14:04:50 +0300

    Refactor constructor

--- tdd-mooc-gonway/src/Shape.mjs
+++ tdd-mooc-gonway/src/Shape.mjs
@@ -4,17 +4,23 @@ export class Shape {
 
   constructor(string) {
     this.#shape = string ?? "";
-    this.#coordinates = this.#shape
+    this.#coordinates = this.setCoordinates();
+  }
+
+  setCoordinates() {
+    return this.#shape
       .replaceAll("!", "")
       .split("$")
       .map((row) => row.split(""))
-      .map((row, ri) => row
-        .map((col, ci) => {
-          if (col === "o") return ri * 3 + ci
-          return col
-        })
-        .filter((col) => col !== "b")
-    ).flat().toString();
+      .map((row, ri) =>
+        row
+          .map((col, ci) => {
+            if (col === "o") return ri * 3 + ci;
+            return col;
+          })
+          .filter((col) => col !== "b")
+      )
+      .flat();
   }
 
   getCoordinates() {
