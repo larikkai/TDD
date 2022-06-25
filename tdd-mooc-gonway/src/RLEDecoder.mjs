9355f920f017f351225a9d41161fb1a786269a01
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-25 14:43:19 +0300

    Update decoder

--- tdd-mooc-gonway/src/RLEDecoder.mjs
+++ tdd-mooc-gonway/src/RLEDecoder.mjs
@@ -1,3 +1,17 @@
 export function decode(pattern) {
-  return "oo&oo!";
+  const decodedPattern = [];
+  const arr = pattern.split("");
+  let count = 1;
+  arr.forEach((c) => {
+    if (!isNaN(c)) count = Number(c);
+    else {
+      while (count > 0) {
+        count--;
+        if (count > 1) arr.push(count);
+        decodedPattern.push(c);
+      }
+      count = 1;
+    }
+  });
+  return decodedPattern.join("");
 }
