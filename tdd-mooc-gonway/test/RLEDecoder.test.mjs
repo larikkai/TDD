4f37c97490c094814e70f3ebb5fab1e772457dd2
Kai Larikka <kai.larikka@kolumbus.fi>
2022-06-25 14:51:23 +0300

    Add moar decoder tests

--- tdd-mooc-gonway/test/RLEDecoder.test.mjs
+++ tdd-mooc-gonway/test/RLEDecoder.test.mjs
@@ -2,8 +2,18 @@ import { expect } from "chai";
 import { decode } from "../src/RLEDecoder.mjs";
 
 describe("Decoding RLE format", () => {
-  it("Decoding RLE pattern", () => {
+  it("Decoding RLE block pattern", () => {
     const decoded = decode("2o$2o!");
-    expect(decoded).to.equal("oo&oo!");
+    expect(decoded).to.equal("oo$oo!");
+  });
+
+  it("Decoding RLE blinker pattern", () => {
+    const decoded = decode("3o!");
+    expect(decoded).to.equal("ooo!");
+  });
+
+  it("Decoding RLE glider pattern", () => {
+    const decoded = decode("bob$2bo$3o!");
+    expect(decoded).to.equal("bob$bbo$ooo!");
   });
 });
